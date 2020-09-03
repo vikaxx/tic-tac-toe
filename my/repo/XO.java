52205e34185e5933d7d77f7df63693ca6a36a627
vikaxx <victoria.aliaeva@gmail.com>
2020-09-03 20:07:24 +0300

    not allow alpha input

--- my/repo/XO.java
+++ my/repo/XO.java
@@ -34,34 +34,34 @@ public class XO {
     }
 
     private static int inputRow(BufferedReader reader) {
-        int row = 0;
+        int row = 10;
         System.out.print("Введите номер строки: ");
         try {
             row = Integer.parseInt(reader.readLine()) - 1;
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (Exception e) {
+//            e.printStackTrace();
         }
-        if (row > 2 || row < 0) {
+        if (row <= 2 && row >= 0) {
+            return row;
+        } else {
             System.out.println("Ошибка. Выберите строку от 1 до 3");
             return inputRow(reader);
-        } else {
-            return row;
         }
     }
 
     private static int inputCol(BufferedReader reader) {
-        int col = 0;
+        int col = 10;
         System.out.print("Введите номер столбца: ");
         try {
             col = Integer.parseInt(reader.readLine()) - 1;
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (Exception e) {
+//            e.printStackTrace();
         }
-        if (col > 2 || col < 0) {
+        if (col <= 2 && col >= 0) {
+            return col;
+        } else {
             System.out.println("Ошибка. Выберите столбец от 1 до 3");
             return inputCol(reader);
-        } else {
-            return col;
         }
     }
 
@@ -73,10 +73,10 @@ public class XO {
         switch (gameField.getCellState(row, col)) {
             case X:
                 System.out.println("Клетка занята. Выберите другую.");
-                gameProcess(reader, gameField, isX);
+                return gameProcess(reader, gameField, isX);
             case O:
                 System.out.println("Клетка занята. Выберите другую.");
-                gameProcess(reader, gameField, isX);
+                return gameProcess(reader, gameField, isX);
             default:
                 if (isX) {
                     gameField.changeCellStateTo(row, col, X);
